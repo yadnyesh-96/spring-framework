e008b846a6e8b4dee7f3f6557b64215db058bde2
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-09-18 02:16:49 +0530

    Employee HR Management Portal app project

---
+++ 03-spring-MVC/01_Employee HR Management Portal/src/main/java/com/employee/config/WebAppInitializer.java
@@ -0,0 +1,21 @@
+package com.employee.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebMvcConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+}
---
+++ 03-spring-MVC/01_Employee HR Management Portal/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Employee HR Portal</title>
+</head>
+<body>
+
+	<h1>Employee HR Management Portal</h1>
+
+	<p>Welcome to the Employee Management System.</p>
+
+</body>
+</html>
